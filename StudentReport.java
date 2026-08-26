04d3dd3fcd6c87e84794d928423215f13ecb8ef3
Nikolai Suarez <nikolai.suarez@tecsup.edu.pe>
2026-08-25 21:01:34 -0500

    Crear StudentModel y StudentReport

---
+++ StudentReport.java
@@ -0,0 +1,7 @@
+﻿public class StudentReport {
+
+    public void showStudent(StudentModel student) {
+        System.out.println("Nombre: " + student.getName());
+        System.out.println("Edad: " + student.getAge());
+    }
+}
